6183b02ae51c652bc4e33cf2b9d95206bd83b8c0
Shweata N Hegde <shweata.hegde@gmail.com>
2021-08-09 16:32:47 +0530

    cleaned the code

--- demo.py
+++ demo.py
@@ -2,10 +2,10 @@ import os
 from docanalysis import DocAnalysis
 ethic_statement_creator = DocAnalysis()
 dict_for_entities = ethic_statement_creator.extract_entities_from_papers(
-    "essential oil AND chemical composition",
-    100,
+    "ethics statement",
+    10,
     os.path.join(
-        os.getcwd(), "corpus", "e_cancer_clinical_trial_50",
+        os.getcwd(), "corpus", "ethics_statement_2",
     ),
     os.path.join(
         os.getcwd(), "ethics_dictionary", "ethics_key_phrases", "ethics_key_phrases.xml"
