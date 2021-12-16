3bd712c6454c1432da6801126f2e31bfbf352e8c
Shweata N Hegde <shweata.hegde@gmail.com>
2021-12-16 21:23:25 +0530

    deleted multiple scripts; merged all code into one single script

--- docanalysis/__init__.py
+++
@@ -1,1 +0,0 @@
-from docanalysis.docanalysis import DocAnalysis
--- docanalysis/download_pre_process.py
+++
@@ -1,5 +0,0 @@
-import os
-class DownloadPapers:
-    def create_project_files(self, QUERY, HITS, OUTPUT):
-        os.system(f'pygetpapers -q "{QUERY}" -k {HITS} -o {OUTPUT} -x')
-        os.system(f"ami -p {OUTPUT} section")
