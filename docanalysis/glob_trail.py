b0de110063b309a3b47f58415f235d2ebcf33ed3
Shweata N. Hegde <70576776+ShweataNHegde@users.noreply.github.com>
2022-03-09 12:19:12 +0530

    glob list

---
+++ docanalysis/glob_trail.py
@@ -0,0 +1,26 @@
+import os
+from glob import glob
+from pprint import pprint
+
+# define constants
+ABS = ['*abstract.xml']
+ACK = ['*ack.xml']
+AFF = ['*aff.xml']
+AUT = ['*contrib-group.xml']
+CON = ['*conclusion*/*.xml']
+DIS = ['*discussion*/**/*_title.xml', '*discussion*/**/*_p.xml'] # might bring unwanted sections like tables, fig. captions etc. Maybe get only title and paragraphs?
+ETH = ['*ethic*/*.xml']
+FIG = ['*fig*.xml']
+INT = ['*introduction*/*.xml', '*background*/*.xml']
+KEY = ['*kwd-group.xml']
+MET = ['*method*/*.xml', '*material*/*.xml'] # also gets us supplementary material. Not sure how to exclude them
+RES = ['*result*/*/*_title.xml', '*result*/*/*_p.xml'] # not sure if we should use recursive globbing or not. 
+TAB = ['*table*.xml']
+TIL = ['*article-meta/*title-group.xml']
+
+path = os.getcwd()
+LIST_SEC = [TIL, KEY]
+for SEC in LIST_SEC:
+    for opt in SEC:
+        glob_list=glob(os.path.join(path,'glob_trial_terpenes', '**', 'sections', '**', f'{opt}'), recursive=True)
+        pprint(glob_list)
