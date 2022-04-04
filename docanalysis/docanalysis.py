db79346e3da2394479a7e6c820d1ef65e9722cc4
Ayush Garg <themoviechannel66@gmail.com>
2022-04-04 14:54:10 +0800

    added few fixes

--- docanalysis/docanalysis.py
+++ docanalysis/docanalysis.py
@@ -92,8 +92,8 @@ class Docanalysis:
         parser.add_argument(
             "-o",
             "--output",
-            default="entities.csv",
-            help="outputs csv file [default=entities.csv]",
+            default=False,
+            help="outputs csv file",
         )
         parser.add_argument(
             "--make_ami_dict",
@@ -122,7 +122,7 @@ class Docanalysis:
 
         parser.add_argument(
             "--spacy_model",
-            default="spacy",
+            default=False,
             type=str,
             help="optional. Choose between spacy or scispacy models. Defaults to spacy",
         )
@@ -166,7 +166,7 @@ class Docanalysis:
                 vars(args)[arg] = False
         self.handle_logger_creation(args)
         self.entity_extraction.extract_entities_from_papers(args.project_name,args.dictionary,search_section=args.search_section,entities=args.entities,query=args.query,hits=args.hits,
-                                     run_pygetpapers=args.run_pygetpapers, make_section= args.make_section, removefalse=True, create_csv=True,
+                                     run_pygetpapers=args.run_pygetpapers, make_section= args.make_section, removefalse=True,
                                      csv_name=args.output,make_ami_dict=args.make_ami_dict,spacy_model=args.spacy_model,html_path=args.html, synonyms=args.synonyms)
 
 
